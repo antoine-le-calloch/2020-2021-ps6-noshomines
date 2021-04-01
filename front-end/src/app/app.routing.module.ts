166fbf6bc510022f1a19faa787c57837ff31a94e
NaudEric <eric.naud@etu.unice.fr>
2021-04-01 15:48:34 +0200

    #6 ajout component option inutile

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -4,13 +4,15 @@ import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 import {PlayQuizComponent} from './quizzes/play-quiz/play-quiz.component';
+import {OptionQuizComponent} from './quizzes/option-quiz/option-quiz.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'play-quiz/:id', component: PlayQuizComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
-    { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
+    {path: 'option-quiz', component: OptionQuizComponent},
+    {path: '', redirectTo: '/quiz-list', pathMatch: 'full'},
 ];
 
 @NgModule({
