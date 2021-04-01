1573bb0a52410e72854b8aef333a65c4fdb27908
NaudEric <eric.naud@etu.unice.fr>
2021-04-01 11:23:47 +0200

    #5 back attempt

---
+++ back-end/app/models/attempt.model.js
@@ -0,0 +1,8 @@
+const Joi = require('joi')
+const BaseModel = require('../utils/base-model.js')
+
+module.exports = new BaseModel('Attempt', {
+  quizId: Joi.number().required(),
+  userId: Joi.number().required(),
+  usersAnswers: Joi.array(),
+})
