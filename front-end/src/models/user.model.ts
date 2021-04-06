ef2dc7cb1016683b948be668bd966b2722ebd10d
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2021-04-06 23:25:43 +0200

    #4 ajout de css sur la permière page, ajout de route vers la création d'un user. ataptation des couleurs pour une meilleurs lisibilité des patient ajout des date de naissance, commentaire et phatologye. création de flexbox pour l'affichage de tous les user

--- front-end/src/models/user.model.ts
+++ front-end/src/models/user.model.ts
@@ -2,5 +2,8 @@ export interface User {
     id: string;
     firstName: string;
     lastName: string;
+    birthday: string;
+    comment: string;
+    pathology: string;
     restartQuestion: boolean;
 }
