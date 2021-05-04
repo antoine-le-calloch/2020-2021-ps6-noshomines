6e012142ddfa904b4e9bde55dd78f79c4dbace69
NaudEric <eric.naud@etu.unice.fr>
2021-05-04 11:41:47 +0200

    #26 correction bug + de 4 rep

--- front-end/src/app/questions/question-form/question-form.component.ts
+++ front-end/src/app/questions/question-form/question-form.component.ts
@@ -53,6 +53,7 @@ export class QuestionFormComponent implements OnInit {
   addQuestion(): void {
     if (this.questionForm.valid) {
       const question = this.questionForm.getRawValue() as Question;
+      console.log(question.answers.length);
       this.quizService.addQuestion(this.quiz, question);
       console.log(this.questionForm.value);
       this.initializeQuestionForm();
